f4e81e26d6437557c91a4d5735297b9bf189816e
ohtaehyun <othdev95@gmail.com>
2025-07-07 04:16:36 +0900

    feat: 티커 존재여부를 확인하는 코드 추가

--- src/stock/application/service/create-stock.service.ts
+++ src/stock/application/service/create-stock.service.ts
@@ -4,6 +4,7 @@ import { CreateStockCommandMapper } from '@/stock/application/mapper/create-stoc
 import { StockEntity } from '@/stock/domain/entity/stock.entity';
 import { STOCK_ERROR_CODE } from '@/stock/domain/error/stock-error';
 import { IStockRepository } from '@/stock/domain/repository/stock.repository';
+import { IYahooFinanceRepository } from '@/yahoo-finance/domain/repository/yahoo-finance.repository';
 import { Inject, Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -11,10 +12,23 @@ export class CreateStockService {
   constructor(
     @Inject(IStockRepository)
     private readonly stockRepository: IStockRepository,
+    @Inject(IYahooFinanceRepository)
+    private readonly yahooFinanceRepository: IYahooFinanceRepository,
   ) {}
 
   async execute(command: CreateStockCommand): Promise<StockEntity> {
     const stock = CreateStockCommandMapper.toEntity(command);
+
+    const isTickerExist = await this.yahooFinanceRepository.isTickerValid(
+      stock.ticker,
+    );
+
+    if (!isTickerExist) {
+      throw new ApplicationException(
+        STOCK_ERROR_CODE.NOT_FOUND_TICKER,
+        '티커를 찾을 수 없습니다.',
+      );
+    }
 
     const exist = await this.stockRepository.findOneByTicker(stock.ticker);
     if (exist) {
@@ -23,6 +37,7 @@ export class CreateStockService {
         '이미 존재하는 티커입니다.',
       );
     }
+
     return await this.stockRepository.save(stock);
   }
 }
--- src/yahoo-finance/infra/yahoo/yahoo-finance.repository.ts
+++ src/yahoo-finance/infra/yahoo/yahoo-finance.repository.ts
@@ -1,6 +1,6 @@
 import { Ticker } from '@/shared/domain/vo/ticker';
 import { IYahooFinanceRepository } from '@/yahoo-finance/domain/repository/yahoo-finance.repository';
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import yahooFinance from 'yahoo-finance2';
 
 @Injectable()
@@ -10,8 +10,22 @@ export class YahooFinanceRepository implements IYahooFinanceRepository {
       await yahooFinance.quoteSummary(ticker.value, { modules: ['price'] });
       return true;
     } catch (error) {
-      console.error(error);
+      this.handleError(error);
       return false;
+    }
+  }
+
+  private handleError(error: any) {
+    if (error instanceof yahooFinance.errors.FailedYahooValidationError) {
+      // See the validation docs for examples of how to handle this
+      // error.result will be a partially validated / coerced result.
+      return;
+    } else if (error instanceof yahooFinance.errors.HTTPError) {
+      console.error(`Skipping yf HTTPError: [${error.name}] ${error.message}`);
+      throw error;
+    } else {
+      console.warn(`Skipping yf Error: [${error.name}] ${error.message}`);
+      return;
     }
   }
 }
